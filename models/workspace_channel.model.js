19b80c6e3a686034bf0063b96f12cb5486c32cce
Lucas Caro <lucascaro2010@gmail.com>
2026-01-27 12:02:18 -0300

    Channel flujo

--- models/workspace_channel.model.js
+++ models/workspace_channel.model.js
@@ -1,35 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const workspace_channelSchema = new mongoose.Schema(
+const channelSchema = new mongoose.Schema(
     {
-        id: {
-            type: Number,
-            unique: true
-        },
-        fk_id_workspace: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Workspace',
-            required: true
-        },
-        created_at: {
-            type: Date,
-            default: Date.now
-        },
-        active: {
-            type: Boolean,
-            default: true
-        },
-        name: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: false
-        }
+    fk_id_workspace: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Workspace',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
     }
 )
 
-const Workspace_Channel = mongoose.model ('Workspace Channel', userSchema)
-
-export default Workspace_Channel
+const Channel = mongoose.model('Channel', channelSchema)
+export default Channel
--- routes/workspace.router.js
+++ routes/workspace.router.js
@@ -2,18 +2,36 @@ import express from "express";
 import workspaceController from "../controllers/workspace.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import workspaceMiddleware from "../middlewares/workspace.middleware.js";
+import { channelController } from "../controllers/channel.controller.js";
 
 const workspaceRouter = express.Router()
 
 workspaceRouter.get('/', authMiddleware, workspaceController.getWorkspaces)
 workspaceRouter.post('/', authMiddleware, workspaceController.create)
+
+workspaceRouter.get('/:workspace_id', authMiddleware, workspaceMiddleware(), workspaceController.getById)
+
 workspaceRouter.delete('/:workspace_id', authMiddleware, workspaceController.delete)
 workspaceRouter.post(
     '/:workspace_id/members', 
     authMiddleware, 
-    workspaceMiddleware(['owner', 'admin']), 
+    workspaceMiddleware(['Owner', 'Admin']), 
     workspaceController.addMemberRequest
 )
 workspaceRouter.get('/:workspace_id/members/accept-invitation', workspaceController.acceptInvitation)
+
+workspaceRouter.get(
+    '/:workspace_id/channels',
+    authMiddleware,
+    workspaceMiddleware(),
+    channelController.getAllByWorkspaceId
+)
+
+workspaceRouter.post(
+    '/:workspace_id/channels',
+    authMiddleware,
+    workspaceMiddleware(['Owner', 'Admin']),
+    channelController.create
+)
 
 export default workspaceRouter
